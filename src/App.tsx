c86604dcb4ca7fc81c7ed7f81cf8d5096699963a
ipavka <ipavka@ya.ru>
2022-01-27 15:38:36 +0300

    lesson 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className={'app-wrapper'}>
-            <header className={'header'}>
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST9Bj04UUgRET0JT4J8kZ3Lq60KlDUrrUAuA&usqp=CAU"
-                    alt="logo"/>
-            </header>
-            <nav className={'nav'}>
-                <div>
-                    <a href="#">Profile</a>
-                </div>
-                <div>
-                    <a href="#">Messages</a>
-                </div>
-                <div>
-                    <a href="#">News</a>
-                </div>
-                <div>
-                    <a href="#">Music</a>
-                </div>
-                <div>
-                    <a href="#">Settings</a>
-                </div>
-            </nav>
-            <div className={'content'}>
-                <div>
-                    <img
-                        src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>
-                            post 1
-                        </div>
-                        <div>
-                            post 2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+      <header className={'header'}>
+          <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST9Bj04UUgRET0JT4J8kZ3Lq60KlDUrrUAuA&usqp=CAU"
+              alt="logo"/>
+      </header>
+  )
+}
+
+export default Header;
