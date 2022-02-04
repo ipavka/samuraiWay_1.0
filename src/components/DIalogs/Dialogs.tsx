dda6aa5c02eef75ed2680dc11dc362881bd66086
ipavka <ipavka@ya.ru>
2022-02-04 18:12:03 +0300

    lesson 21

--- src/components/DIalogs/Dialogs.tsx
+++ src/components/DIalogs/Dialogs.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import dialogsStyle from './Dialogs.module.css';
+import dStyle from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={dStyle.dialogs}>
+            <div className={dStyle.dialogsItems}>
+                <div className={dStyle.dialog}>Dimon</div>
+                <div className={dStyle.dialog}>Andrey</div>
+                <div className={dStyle.dialog}>Max</div>
+                <div className={dStyle.dialog}>Svetlana</div>
+                <div className={dStyle.dialog}>Elena</div>
+                <div className={dStyle.dialog}>Pavel</div>
+            </div>
+            <div className={dStyle.messages}>
+                <div className={dStyle.msg}>Hi!!!</div>
+                <div className={dStyle.msg}>Yep!!</div>
+                <div className={dStyle.msg}>Goodbye</div>
+            </div>
         </div>
     )
 }
