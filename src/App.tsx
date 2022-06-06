47ad44838775dd1f1900d8f2f4d8afabd3d4ee52
ipavka <ipavka@ya.ru>
2022-06-06 19:15:01 +0300

    lesson_94 lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import s from './App.module.css';
 import {Route, withRouter} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import {FriendsItems} from "./components/Friends/FriendsItems/FriendsItems";
-import DialogsContainer from "./components/DIalogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import {Login} from "./components/Login/Login";
 import HeaderContainer from "./components/Header/HeaderContainer";
@@ -17,55 +15,61 @@ import {MySpinner} from "./components/common/MySpinner/MySpinner";
 import {setInitializationThunkCreator} from "./redux/app-reducer";
 import {NavContainer} from "./components/Navbar/NavContainer";
 
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+
+
 class App extends React.Component<AppPropsType> {
 
-    componentDidMount() {
-        this.props.setInitializationApp()
-    }
+  componentDidMount() {
+    this.props.setInitializationApp()
+  }
 
-    render() {
-        if (!this.props.initialization) {
-            return <MySpinner/>
-        }
-        return (
-            <div className={s.appWrapper}>
-                <HeaderContainer/>
-                <main className={s.appWrapperContent}>
-                    <NavContainer/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                    <Route exact path='/' render={() => <ProfileContainer/>}/>
-                    <Route path='/dialog' render={() => <DialogsContainer/>}/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
-                    <Route path='/settings' render={() => <Settings/>}/>
-                    <Route path='/friends' render={() => <FriendsItems/>}/>
-                    <Route path='/users' render={() => <UsersContainer/>}/>
-                    <Route path='/login' render={() => <Login/>}/>
-                </main>
-            </div>
-        );
+  render() {
+    if (!this.props.initialization) {
+      return <MySpinner/>
     }
+    return (
+      <div className={s.appWrapper}>
+        <HeaderContainer/>
+        <main className={s.appWrapperContent}>
+          <NavContainer/>
+          <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+          <Route exact path='/' render={() => <ProfileContainer/>}/>
+          <Route path='/news' render={() => <News/>}/>
+          <Route path='/music' render={() => <Music/>}/>
+          <Route path='/settings' render={() => <Settings/>}/>
+          <Route path='/friends' render={() => <FriendsItems/>}/>
+          <Suspense fallback={<MySpinner/>}>
+            <Route path='/users' render={() => <UsersContainer/>}/>
+            <Route path='/dialog' render={() => <DialogsContainer/>}/>
+          </Suspense>
+          <Route path='/login' render={() => <Login/>}/>
+        </main>
+      </div>
+    );
+  }
 }
 
 type MapStateToPropsType = {
-    initialization: boolean
+  initialization: boolean
 }
 type MapDispatchPropsType = {
-    setInitializationApp: () => void
+  setInitializationApp: () => void
 }
 export type AppPropsType = MapStateToPropsType & MapDispatchPropsType
 
 const mapStateToProps = (state: AppStateType) => {
-    return {
-        initialization: state.app.initialized
-    }
+  return {
+    initialization: state.app.initialized
+  }
 }
 
 export default compose<React.FC>(
-    withRouter,
-    connect(mapStateToProps,
-        {
-            setInitializationApp: setInitializationThunkCreator
-        }
-    )
+  withRouter,
+  connect(mapStateToProps,
+    {
+      setInitializationApp: setInitializationThunkCreator
+    }
+  )
 )(App)
