9a530eb558e9d0e87d84a478c8267e2dd1a10137
ipavka <ipavka@ya.ru>
2022-08-31 14:54:31 +0300

    change data master

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import settingsStyle from './Settings.module.css';
 
+const data = [
+  {
+    id: 1,
+    name: 'Dimon'
+  },
+  {
+    id: 2,
+    name: 'Max'
+  },
+]
+
 export const Settings = () => {
     return (
         <div>
             Settings
+          <div>
+            {data.map(el => {
+              return (
+                <div>
+                  <p>{el.id}</p>
+                  <p>{el.name}</p>
+                </div>
+              )
+            })}
+          </div>
         </div>
     )
 }
