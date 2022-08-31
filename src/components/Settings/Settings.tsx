60a7e4d4e31924a56cb641908c95796f128d2366
ipavka <ipavka@ya.ru>
2022-08-31 15:55:58 +0300

    feature-2 change-1

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -19,32 +19,35 @@ const data = [
     id: 4,
     name: 'master-cahge'
   },
-
+  {
+    id: 5,
+    name: 'Fill'
+  },
 ]
 
 export const Settings = () => {
-    return (
-        <div>
-            Settings
-          <div>
-            {data.map(el => {
-              return (
-                <div>
-                  <p>{el.id}</p>
-                  <p>{el.name}</p>
-                </div>
-              )
-            })}
-          </div>
-        <h3>master-change-5</h3>
-          <h1>feature-1-change-3</h1>
-          <h1>feature-1-change-4</h1>
-          <div>
-            <h1>feature-1-change-5</h1>
-          </div>
-          <div>
-            <h2>feature-1-change-6</h2>
-          </div>
-        </div>
-    )
+  return (
+    <div>
+      Settings
+      <div>
+        {data.map(el => {
+          return (
+            <div>
+              <p>{el.id}</p>
+              <p>{el.name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <h3>master-change-5</h3>
+      <h1>feature-1-change-3</h1>
+      <h1>feature-1-change-4</h1>
+      <div>
+        <h1>feature-1-change-5</h1>
+      </div>
+      <div>
+        <h2>feature-1-change-6</h2>
+      </div>
+    </div>
+  )
 }
